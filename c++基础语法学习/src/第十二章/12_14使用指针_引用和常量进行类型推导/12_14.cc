02d45e2a0e2d97f7773534f1b40cd84a56ac8bf1
hanbo <hanbo2393954254@gmail.com>
2023-10-27 16:32:19 +0800

    20231027

---
+++ c++基础语法学习/src/第十二章/12_14使用指针_引用和常量进行类型推导/12_14.cc
@@ -0,0 +1,37 @@
+#include<iostream>
+#include <string>
+#include<cassert>
+
+
+//类型推导会 丢弃 const 和 constexpr关键字
+
+//类型推导会 丢弃 引用 &
+
+std::string& getRef();
+auto ref{getRef()}; //推导为std::string 而不是 std::string&
+//如果还想推导为 引用 的话 就在前面自己加上
+auto &ref1{getRef()};
+
+
+//顶级常量和低级常量：
+//顶级常量
+//顶级常量是适用于对象本身的常量限定符
+const int x;
+int *const ptr; // 指针本身是const
+
+//低级常量
+//低级 const 是适用于被引用或指向的对象的 const 限定符：
+const int &ref;
+const int *ptr1; //指针指向const对象
+
+//对常量值的引用始终是低级常量。指针可以具有顶级、低级或两种类型的常量：
+const int *const ptr3;//左侧的const是低级，右侧的const是高级
+
+
+//常量推导和常量引用
+
+
+int main()
+{
+
+}
