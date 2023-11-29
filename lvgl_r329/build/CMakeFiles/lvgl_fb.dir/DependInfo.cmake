236b5721f655ee7f8befee61eb184369ec705000
hanbo <hanbo2393954254@gmail.com>
2023-11-29 21:15:18 +0800

    111

---
+++ lvgl_r329/build/CMakeFiles/lvgl_fb.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/hansolo/CplusplusLearn/lvgl_r329/lv_port_linux_frame_buffer/lv_port_indev_hansolo.c" "CMakeFiles/lvgl_fb.dir/lv_port_indev_hansolo.c.o" "gcc" "CMakeFiles/lvgl_fb.dir/lv_port_indev_hansolo.c.o.d"
+  "/home/hansolo/CplusplusLearn/lvgl_r329/lv_port_linux_frame_buffer/mouse_cursor_icon.c" "CMakeFiles/lvgl_fb.dir/mouse_cursor_icon.c.o" "gcc" "CMakeFiles/lvgl_fb.dir/mouse_cursor_icon.c.o.d"
+  "/home/hansolo/CplusplusLearn/lvgl_r329/lv_port_linux_frame_buffer/project/demo/main.c" "CMakeFiles/lvgl_fb.dir/project/demo/main.c.o" "gcc" "CMakeFiles/lvgl_fb.dir/project/demo/main.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/hansolo/CplusplusLearn/lvgl_r329/build/lvgl/CMakeFiles/lvgl.dir/DependInfo.cmake"
+  "/home/hansolo/CplusplusLearn/lvgl_r329/build/lvgl/CMakeFiles/lvgl_examples.dir/DependInfo.cmake"
+  "/home/hansolo/CplusplusLearn/lvgl_r329/build/lvgl/CMakeFiles/lvgl_demos.dir/DependInfo.cmake"
+  "/home/hansolo/CplusplusLearn/lvgl_r329/build/lv_drivers/CMakeFiles/lv_drivers.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lvgl_r329/build/CMakeFiles/lvgl_fb.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lvgl_fb.dir/lv_port_indev_hansolo.c.o"
+  "CMakeFiles/lvgl_fb.dir/lv_port_indev_hansolo.c.o.d"
+  "CMakeFiles/lvgl_fb.dir/mouse_cursor_icon.c.o"
+  "CMakeFiles/lvgl_fb.dir/mouse_cursor_icon.c.o.d"
+  "CMakeFiles/lvgl_fb.dir/project/demo/main.c.o"
+  "CMakeFiles/lvgl_fb.dir/project/demo/main.c.o.d"
+  "lvgl_fb"
+  "lvgl_fb.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lvgl_fb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
